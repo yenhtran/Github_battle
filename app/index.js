3b01ec60875b4948b852c2de608f06b8d3d38b74
Yen Tran <ytran@marinsoftware.com>
2019-12-08 23:16:29 -0800

    navbar

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import './index.css'
+import Popular from './components/Popular'
 
 class App extends React.Component {
   render() {
@@ -13,8 +14,8 @@ class App extends React.Component {
        *    'Hello World!'
        *  )
        */
-      <div>
-        Hello World!
+      <div className='container'>
+        <Popular />
       </div>
     )
   }
